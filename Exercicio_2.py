0c3148f017f51838b9eedaef188e210efbc57f47
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2025-05-21 15:44:17 +0000

    Atividade 3

---
+++ Exercicio_2.py
@@ -0,0 +1,12 @@
+palavra = input("Digite uma palavra: ")
+
+
+vogais = 'aeiouAEIOU'
+
+contador_vogais = 0
+
+for letra in palavra:
+    if letra in vogais:
+        contador_vogais += 1  
+
+print(f"Total de vogais: {contador_vogais}")
---
+++ Exercicio|_1.py
@@ -0,0 +1,5 @@
+i = int(input("Digite um número: "))
+
+while i >= 0:
+    print(X) 
+    i -= 1
